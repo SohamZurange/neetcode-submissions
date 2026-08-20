7cb9807dd6bdefc1844c3f008dfff482a987ef8e
Soham Zurange <54995067+SohamZurange@users.noreply.github.com>
2026-08-20 18:34:58 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int trap(int[] height) {
+        int l = 0;
+        int r = height.length-1;
+        int lMax =0;
+        int rMax =0;
+        int ans = 0;
+
+        while(l < r){
+            lMax = Math.max(lMax, height[l]);
+            rMax = Math.max(rMax, height[r]);
+            if(lMax < rMax){
+                ans += (lMax - height[l]);
+                l++;
+            }else{
+                ans += (rMax - height[r]);
+                r--;
+            }
+        }
+        return ans;
+    }
+}
